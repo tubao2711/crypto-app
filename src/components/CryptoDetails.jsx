31783e28260d87a83dfaed99ef0cada304cd12b1
tubao2711 <bao0975448317@gmail.com>
2024-04-19 21:26:55 +0700

    commit_final3

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import HTMLReactParser from "html-react-parser";
-import { useParams } from "react-router-dom";
-import { LineChart } from "../components";
-import millify from "millify";
-import { Col, Row, Typography, Select } from "antd";
+import React, { useState } from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import { LineChart } from '../components';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
 import {
   MoneyCollectOutlined,
   DollarCircleOutlined,
@@ -14,48 +14,48 @@ import {
   CheckOutlined,
   NumberOutlined,
   ThunderboltOutlined,
-} from "@ant-design/icons";
+} from '@ant-design/icons';
 
 import {
   useGetCryptoDetailsQuery,
   useGetCryptoHistoryQuery,
-} from "../services/cryptoApi";
+} from '../services/cryptoApi';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
 
 const CryptoDetails = () => {
   const { coinId } = useParams();
-  const [timeperiod, setTimeperiod] = useState("7d");
-  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  const [timeperiod, setTimeperiod] = useState('7d');
+  const { data } = useGetCryptoDetailsQuery(coinId);
   const { data: coinHistory } = useGetCryptoHistoryQuery({
     coinId,
     timeperiod,
   });
   const cryptoDetails = data?.data?.coin;
-  const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
+  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
   const stats = [
     {
-      title: "Price to USD",
+      title: 'Price to USD',
       value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
       icon: <DollarCircleOutlined />,
     },
-    { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+    { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
     {
-      title: "24h Volume",
+      title: '24h Volume',
       value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
       icon: <ThunderboltOutlined />,
     },
     {
-      title: "Market Cap",
+      title: 'Market Cap',
       value: `$ ${
         cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
       }`,
       icon: <DollarCircleOutlined />,
     },
     {
-      title: "All-time-high(daily avg.)",
+      title: 'All-time-high(daily avg.)',
       value: `$ ${
         cryptoDetails?.allTimeHigh?.price &&
         millify(cryptoDetails?.allTimeHigh?.price)
@@ -66,17 +66,17 @@ const CryptoDetails = () => {
 
   const genericStats = [
     {
-      title: "Number Of Markets",
+      title: 'Number Of Markets',
       value: cryptoDetails?.numberOfMarkets,
       icon: <FundOutlined />,
     },
     {
-      title: "Number Of Exchanges",
+      title: 'Number Of Exchanges',
       value: cryptoDetails?.numberOfExchanges,
       icon: <MoneyCollectOutlined />,
     },
     {
-      title: "Aprroved Supply",
+      title: 'Aprroved Supply',
       value: cryptoDetails?.supply?.confirmed ? (
         <CheckOutlined />
       ) : (
@@ -85,14 +85,14 @@ const CryptoDetails = () => {
       icon: <ExclamationCircleOutlined />,
     },
     {
-      title: "Total Supply",
+      title: 'Total Supply',
       value: `$ ${
         cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
       }`,
       icon: <ExclamationCircleOutlined />,
     },
     {
-      title: "Circulating Supply",
+      title: 'Circulating Supply',
       value: `$ ${
         cryptoDetails?.supply?.circulating &&
         millify(cryptoDetails?.supply?.circulating)
@@ -176,7 +176,7 @@ const CryptoDetails = () => {
           <Title level={3} className="coin-details-heading">
             What is {cryptoDetails?.name}?
           </Title>
-          {typeof cryptoDetails?.description === "string"
+          {typeof cryptoDetails?.description === 'string'
             ? HTMLReactParser(cryptoDetails.description)
             : null}
         </Row>
